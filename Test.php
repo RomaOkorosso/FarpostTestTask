f55cae031c61a27922a6617d4661aa998b9f4889
RomaOkorosso <okorosso.roma.work@gmail.com>
2021-08-22 09:38:59 +1000

    add tests

--- Test.php
+++ Test.php
@@ -6,18 +6,52 @@ require_once 'analyze.php';
 
 class Test extends TestCase
 {
-    public function testOne()
+    public function testLargeData()
     {
-        $expected_result = "14/06/2017:16:48:52 14/06/2017:16:48:52 91.666666666667";
-        $anal = new Analyzer();
-        $res = $anal->analyze(99.9, 45, true, "test1.log");
-
-        echo "\n----------------------------\n";
-        echo $expected_result . "\n";
-        echo $res . "\n";
-        echo "----------------------------\n";
-        $this->assertEquals($expected_result,
-            Analyzer::analyze(99.9, 45, true, "test1.log"));
+        $expected_result = "14/06/2017:16:48:51 14/06/2017:16:49:02 91.666666666667";
+        $res = Analyzer::analyze(99.9, 45, true, "test1.log");
+        $res = str_replace("\n", "", $res);
+        $this->assertEquals($expected_result, $res);
     }
 
+    public function testThreeWrongWithTwoNormal()
+    {
+        $expected_result = "14/06/2017:16:48:52 14/06/2017:16:48:52 014/06/2017:16:48:54 14/06/2017:16:48:56 33.333333333333";
+        $res = Analyzer::analyze(99.9, 45, true, "test2.log");
+        $res = str_replace("\n", "", $res);
+        $this->assertEquals($expected_result, $res);
+    }
+
+    public function testOnly500ResCodes()
+    {
+        $expected_result = "14/06/2017:16:48:52 14/06/2017:16:48:56 0";
+        $res = Analyzer::analyze(99.9, 45, true, "test3.log");
+        $res = str_replace("\n", "", $res);
+        $this->assertEquals($expected_result, $res);
+    }
+
+    public function testMixedAllWrong()
+    {
+        $expected_result = "14/06/2017:16:48:52 14/06/2017:16:48:56 0";
+        $res = Analyzer::analyze(99.9, 45, true, "test4.log");
+        $res = str_replace("\n", "", $res);
+        $this->assertEquals($expected_result, $res);
+    }
+
+    public function testMixed()
+    {
+        $expected_result = '14/06/2017:16:48:52 14/06/2017:16:48:57 5014/06/2017:16:48:59 14/06/2017:16:49:00 50';
+        $res = Analyzer::analyze(99.9, 45, true, "test5.log");
+        $res = str_replace("\n", "", $res);
+        $this->assertEquals($expected_result, $res);
+    }
+
+    public function testWrongInMiddle()
+    {
+        $expected_result = "14/06/2017:16:48:52 14/06/2017:16:48:55 50";
+        $res = Analyzer::analyze(99.9, 45, true, "test6.log");
+        $res = str_replace("\n", "", $res);
+        $this->assertEquals($expected_result, $res);
+    }
 }
+
--- analyze.php
+++ analyze.php
@@ -2,19 +2,27 @@
 $allowed_process_time = 0;
 $min_uptime_percents = 0.0;
 $response_count = 0;
-$response_with_errors = 0;
 $is_test = false;
 const MIN_RESPONSE_CODE = 500;
 const MAX_RESPONSE_CODE = 600;
+
+function unset_all_values()
+{
+    $vars = array_keys(get_defined_vars());
+    for ($i = 0; $i < sizeOf($vars); $i++) {
+        echo $vars[$i] . "\n";
+        unset($$vars[$i]);
+    }
+    unset($vars, $i);
+}
+
 if (!empty($argv)) {
     for ($i = 1; $i < count($argv); $i++) {
         if ($argv[$i] === "-u") {
             $min_uptime_percents = floatval($argv[$i + 1]);
-            // echo "-u = ", $allowed_process_time, " type:", gettype($allowed_process_time), "\n";
         }
         if ($argv[$i] === "-t") {
             $allowed_process_time = intval($argv[$i + 1]);
-            // echo "-t = ", $min_uptime_percents, " type:", gettype($min_uptime_percents), "\n";
         }
     }
 }
@@ -22,52 +30,82 @@ if (!empty($argv)) {
 class Analyzer
 {
 
-    static function analyze($min_uptime_percents, $allowed_process_time, $is_test = false,
-                                   $filename = "access.log"): string
+    static function analyze($min_uptime_percents, $allowed_process_time, $is_test = false, $filename = "access.log"):
+    string
     {
+        $start_time = "";
+        $prev_time = "";
+        $curr_time = "";
         $to_return = "";
-        global $response_count, $response_with_errors;
+        $response_with_errors = 0;
+        $response_count = 0;
         if ($file = fopen($filename, "r")) {
-            $entrance_log_time = "";
+            $uptime_with_errs_percents = 100.0;
             while (!feof($file)) {
                 $line = fgets($file);
                 if (empty($line)) {
-                    return "-1";
-                }
-                $data = explode(" ", $line);
-                $response_code = intval($data[8]);
-                $process_time = floatval($data[10]);
-                $response_count++;
-
-                if (empty($entrance_log_time)) {
-                    $entrance_log_time = str_replace("[", "", $data[3]);
-                }
-
-                if (($response_code >= MIN_RESPONSE_CODE && $response_code < MAX_RESPONSE_CODE) ||
-                    ($process_time > $allowed_process_time)) {
-
-                    $response_with_errors++;
-                    $uptime_with_errs_percents = 100 - ($response_with_errors / $response_count) * 100;
-
                     if ($uptime_with_errs_percents < $min_uptime_percents) {
-                        $result = strval($entrance_log_time . " " . str_replace("[", "", $data[3]) . " " .
-                            $uptime_with_errs_percents . "\n");
+                        $result = strval($start_time . " " . $curr_time . " " . $uptime_with_errs_percents . "\n");
                         if (!$is_test) {
                             echo $result;
                         } else {
                             $to_return .= $result;
                         }
                     }
+                    $response_with_errors = 0;
+                    $response_count = 0;
+                    $uptime_with_errs_percents = 100;
+                    return $to_return;
+                }
+                $data = explode(" ", $line);
+                $response_code = intval($data[8]);
+                $process_time = floatval($data[10]);
+
+                $response_count++;
+                if (empty($start_time)) {
+                    $start_time = str_replace("[", "", $data[3]);
+                }
+                if (empty($prev_time)) {
+                    $prev_time = str_replace("[", "", $data[3]);
+                } else {
+                    $prev_time = $curr_time;
+                }
+                $curr_time = str_replace("[", "", $data[3]);
+
+                //echo $start_time . "\n" . $prev_time . "\n" . $curr_time . "\n\n";
+
+
+                if (($response_code >= MIN_RESPONSE_CODE && $response_code < MAX_RESPONSE_CODE) || ($process_time > $allowed_process_time)) {
+                    //echo "\nYES\n";
+                    $response_with_errors++;
+                    $uptime_with_errs_percents = 100 - ($response_with_errors / $response_count) * 100;
+
+                } else if ($uptime_with_errs_percents < $min_uptime_percents) {
+                    //echo "\nNO\n";
+                    $result = strval($start_time . " " . $prev_time . " " . $uptime_with_errs_percents . "\n");
+                    if (!$is_test) {
+                        echo $result;
+                    } else {
+                        $to_return .= $result;
+                    }
+                    $uptime_with_errs_percents = 100.0;
                     $response_count = 0;
                     $response_with_errors = 0;
-                    $entrance_log_time = "";
+                    $start_time = "";
+                    $prev_time = "";
+                    $curr_time = "";
                 }
             }
             fclose($file);
         }
+        $response_with_errors = 0;
+        $response_count = 0;
+        $uptime_with_errs_percents = 100;
         return $to_return;
     }
 }
 
-Analyzer::analyze($min_uptime_percents, $allowed_process_time, false, "test1.log");
-// echo Analyzer::analyze($min_uptime_percents, $allowed_process_time, true, "access.log");
+Analyzer::analyze($min_uptime_percents, $allowed_process_time, false, "test6.log");
+
+//Analyzer::analyze($min_uptime_percents, $allowed_process_time, true, "test2.log");
+
